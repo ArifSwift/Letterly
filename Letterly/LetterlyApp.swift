f9aaa2c52ee3f346227df94f9953b50ac4023f26
Ahmed <m.ahmed.arif>
2025-11-21 15:56:12 +0100

    Initial Commit

---
+++ Letterly/LetterlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterlyApp.swift
+//  Letterly
+//
+//  Created by Ahmad Arif on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct LetterlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LetterlyTests/LetterlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  LetterlyTests.swift
+//  LetterlyTests
+//
+//  Created by Ahmad Arif on 21.11.25.
+//
+
+import Testing
+@testable import Letterly
+
+struct LetterlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
